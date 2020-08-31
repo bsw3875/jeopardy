ec7bbd9a93fbc2285d86912d6baad4acf3577703
barbara_workman <susan.workman90@gmail.com>
2020-08-31 07:54:14 -0400

    Updates

---
+++ precache-manifest.0d966c47fa60e9de4468ad54eaf4155f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0676e568b55e91a8fcc5c987b30e68e",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "7ca7eda1d61121e51f7b",
+    "url": "/jeopardy/static/css/main.e6dc823b.chunk.css"
+  },
+  {
+    "revision": "056d3f22d712a8cf16fd",
+    "url": "/jeopardy/static/js/2.87ffd695.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jeopardy/static/js/2.87ffd695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca7eda1d61121e51f7b",
+    "url": "/jeopardy/static/js/main.c3dfa7cf.chunk.js"
+  },
+  {
+    "revision": "15f80ddce3766636b967",
+    "url": "/jeopardy/static/js/runtime-main.8cd51dd8.js"
+  }
+]);
